83526c3e1a444c80de43236693387a257c9bc648
skeanster <ryanskeans@gmail.com>
2021-11-13 18:42:30 -0800

    changed state structure from an array to an object

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -6,112 +6,104 @@ import Contactpage from './components/Contactpage';
 import CartPage from './components/CartPage';
 
 const RouteSwitch = () => {
-  const [cartState, setcartState] = useState([
-    { products: [] },
-    { totalItems: 0 },
-    { totalCost: 0 },
-  ]);
+  const [cartState, setcartState] = useState({
+    products: [],
+    totalItems: 0,
+    totalCost: 0,
+  });
 
   const isInCart = (product) => {
-    return cartState[0].products.find(({ name }) => name === product);
+    return cartState.products.find(({ name }) => name === product);
   };
 
   const cartAdd = (item) => {
     if (isInCart(item.target.dataset.name) === undefined) {
-      setcartState([
-        {
-          products: cartState[0].products.concat({
-            name: item.target.dataset.name,
-            quantity: 1,
-            cost: item.target.dataset.price,
-          }),
-        },
-        { totalItems: cartState[1].totalItems + 1 },
-        {
-          totalCost:
-            cartState[2].totalCost +
-            Math.round(
-              (Number(item.target.dataset.price) + Number.EPSILON) * 100
-            ) /
-              100,
-        },
-      ]);
+      setcartState({
+        products: cartState.products.concat({
+          name: item.target.dataset.name,
+          quantity: 1,
+          cost: item.target.dataset.price,
+        }),
+
+        totalItems: cartState.totalItems + 1,
+
+        totalCost:
+          cartState.totalCost +
+          Math.round(
+            (Number(item.target.dataset.price) + Number.EPSILON) * 100
+          ) /
+            100,
+      });
     } else {
-      let itemExist = cartState[0].products.find(
+      let itemExist = cartState.products.find(
         ({ name }) => name === item.target.dataset.name
       );
-      let indexOfItem = cartState[0].products.indexOf(itemExist);
-      let tempArray = [...cartState[0].products];
+      let indexOfItem = cartState.products.indexOf(itemExist);
+      let tempArray = [...cartState.products];
       let tempItem = { ...tempArray[indexOfItem] };
       tempItem.quantity = tempItem.quantity + 1;
       tempArray[indexOfItem] = tempItem;
-      setcartState([
-        {
-          products: tempArray,
-        },
-        { totalItems: cartState[1].totalItems + 1 },
-        {
-          totalCost:
-            cartState[2].totalCost +
-            Math.round(
-              (Number(item.target.dataset.price) + Number.EPSILON) * 100
-            ) /
-              100,
-        },
-      ]);
+      setcartState({
+        products: tempArray,
+
+        totalItems: cartState.totalItems + 1,
+
+        totalCost:
+          cartState.totalCost +
+          Math.round(
+            (Number(item.target.dataset.price) + Number.EPSILON) * 100
+          ) /
+            100,
+      });
     }
   };
 
   const cartRemove = (item) => {
     if (isInCart(item.target.dataset.name).quantity > 1) {
-      let itemExist = cartState[0].products.find(
+      let itemExist = cartState.products.find(
         ({ name }) => name === item.target.dataset.name
       );
-      let indexOfItem = cartState[0].products.indexOf(itemExist);
-      let tempArray = [...cartState[0].products];
+      let indexOfItem = cartState.products.indexOf(itemExist);
+      let tempArray = [...cartState.products];
       let tempItem = { ...tempArray[indexOfItem] };
       tempItem.quantity = tempItem.quantity - 1;
       tempArray[indexOfItem] = tempItem;
-      setcartState([
-        {
-          products: tempArray,
-        },
-        { totalItems: cartState[1].totalItems - 1 },
-        {
-          totalCost:
-            cartState[2].totalCost -
-            Math.round(
-              (Number(item.target.dataset.price) + Number.EPSILON) * 100
-            ) /
-              100,
-        },
-      ]);
+      setcartState({
+        products: tempArray,
+
+        totalItems: cartState.totalItems - 1,
+
+        totalCost:
+          cartState.totalCost -
+          Math.round(
+            (Number(item.target.dataset.price) + Number.EPSILON) * 100
+          ) /
+            100,
+      });
     } else {
-      setcartState([
-        {
-          products: cartState[0].products.filter(
-            (product) => product.name !== item.target.dataset.name
-          ),
-        },
-        { totalItems: cartState[1].totalItems - 1 },
-        {
-          totalCost:
-            cartState[2].totalCost -
-            Math.round(
-              (Number(item.target.dataset.price) + Number.EPSILON) * 100
-            ) /
-              100,
-        },
-      ]);
+      setcartState({
+        products: cartState.products.filter(
+          (product) => product.name !== item.target.dataset.name
+        ),
+
+        totalItems: cartState.totalItems - 1,
+
+        totalCost:
+          cartState.totalCost -
+          Math.round(
+            (Number(item.target.dataset.price) + Number.EPSILON) * 100
+          ) /
+            100,
+      });
     }
   };
 
   const checkoutCart = () => {
-    if (cartState[1].totalItems === 0) {
+    if (cartState.totalItems === 0) {
       return;
     } else {
       alert('Thank you for shopping!');
-      setcartState([{ products: [] }, { totalItems: 0 }, { totalCost: 0 }]);
+      setcartState({ products: [], totalItems: 0, totalCost: 0 });
     }
   };
 
@@ -120,28 +112,25 @@ const RouteSwitch = () => {
       <Routes>
         <Route
           path="/web-store"
-          element={<App cartTotal={cartState[1].totalItems} />}
+          element={<App cartTotal={cartState.totalItems} />}
         />
         <Route
           path="/web-store/Productpage"
           element={
-            <Productpage
-              cartTotal={cartState[1].totalItems}
-              cartAdd={cartAdd}
-            />
+            <Productpage cartTotal={cartState.totalItems} cartAdd={cartAdd} />
           }
         />
         <Route
           path="/web-store/Contactpage"
-          element={<Contactpage cartTotal={cartState[1].totalItems} />}
+          element={<Contactpage cartTotal={cartState.totalItems} />}
         />
         <Route
           path="/web-store/CartPage"
           element={
             <CartPage
-              cartTotal={cartState[1].totalItems}
-              productsList={cartState[0].products}
-              totalPrice={cartState[2].totalCost}
+              cartTotal={cartState.totalItems}
+              productsList={cartState.products}
+              totalPrice={cartState.totalCost}
               cartAdd={cartAdd}
               cartRemove={cartRemove}
               checkoutCart={checkoutCart}
