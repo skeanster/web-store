0950c485d03537423dcf40e45fe9a9e1de29692f
skeanster <ryanskeans@gmail.com>
2021-11-14 17:52:07 -0800

    homepage text edit

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,9 +4,7 @@ const Homepage = () => {
   return (
     <div className="homepageContainer">
       <div className="homeTextContainer">
-        <div className="subMainTitle">
-          "Just a solid online store, 9.2/10" -IGN
-        </div>
+        <div className="subMainTitle">Americas #4 Rated Online Store</div>
         <div className="mainTitle">Putting the F in Fashion</div>
         <Link to="/web-store/Productpage">
           <button className="mainShopButton">Shop Now</button>
